b47ad75655b871db16b6e73d354fa711a051ebbd
SlavikKlimov1988 <88817890+SlavikKlimov1988@users.noreply.github.com>
2022-04-08 14:12:32 +0300

    aaaaaaaaaa
    aaaaaaaaaaaaaaa

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = createMarkupGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.addEventListener("click", onGalleryItemClick);
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+   })
+   .join("");
+}
 
 console.log(galleryItems);
